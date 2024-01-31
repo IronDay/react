cb059121b151dd71a637106d61a1df319eb26f16
Djioleu Florian <nsiegningflorian@gmail.com>
2024-01-31 20:12:41 +0100

    implement the logic of products filtering

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, useState, useTransition } from "react";
 import getProducts from "./Products";
 
 interface Props {
@@ -5,17 +6,55 @@ interface Props {
 }
 
 const Product = ({ name }: Props) => {
-  return <p>{name}</p>;
+  const splitedName: string[] = name.split(" ");
+  return (
+    <p className="w-[250px] h-[250px] p-3 border border-gray-950 rounded-xl flex items-center justify-center">
+      {splitedName[0]}
+      <span className="text-2xl ml-2 text-blue-800">{splitedName[1]}</span>
+    </p>
+  );
+};
+
+const products = getProducts();
+
+const filteringProduct = (filterConstraint: string) => {
+  return filterConstraint
+    ? products.filter((product) => product.includes(filterConstraint))
+    : products;
 };
 
 function App() {
-  const products = getProducts();
+  const [isPending, startTransition] = useTransition();
+ 
+  const [filterConstraint, setFilterConstraint] = useState<string>("");
+  
+  const filteredProducts = filteringProduct(filterConstraint);
+
+  const handleFiltering = (e: ChangeEvent<HTMLInputElement>) => {
+    startTransition(() => {
+      setFilterConstraint(e.target.value);
+    });
+  };
 
   return (
-    <div>
-      {products.map((product,index) => (
-        <Product key={index} name={product} />
-      ))}
+    <div className="flex flex-col items-center justify-between gap-2">
+      <div>
+        <label className="block text-xl" htmlFor="search">
+          Search
+        </label>
+        <input
+          id="search"
+          className="w-[450px] h-[35px] border border-stone-200 outline-none p-3 rounded-full mb-1"
+          type="search"
+          value={filterConstraint}
+          onChange={handleFiltering}
+        />
+      </div>
+      <div className="grid grid-cols-4 gap-3">
+        {filteredProducts.map((product, index) => (
+          <Product key={index} name={product} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/Products.ts
+++ src/Products.ts
@@ -1,7 +1,7 @@
 export default function getProducts() {
   const Products: string[] = [];
   for (let i = 0; i < 10_000; i++) {
-    Products.push(`Products ${i}`);
+    Products.push(`Product ${i}`);
   }
 
   return Products;
