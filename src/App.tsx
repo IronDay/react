8f1f8effb77b70a09292b1a3498ddb873e61bd64
Djioleu Florian <nsiegningflorian@gmail.com>
2024-02-02 11:57:37 +0100

    Implement the Accordions test

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,83 @@
-import { ChangeEvent, useMemo, useState, useTransition } from "react";
-import getProducts from "./Products";
+import { v4 as uuid4 } from "uuid";
+import Accordions from "./components/Accordion.tsx";
 
-interface Props {
-  name: string;
-}
-
-const Product = ({ name }: Props) => {
-  const splitedName: string[] = name.split(" ");
-  return (
-    <p className="w-[250px] h-[250px] p-3 border border-gray-950 rounded-xl flex items-center justify-center">
-      {splitedName[0]}
-      <span className="text-2xl ml-2 text-blue-800">{splitedName[1]}</span>
-    </p>
-  );
-};
-
-const ProductList = ({
-  products,
-  isPending,
-}: {
-  products: string[];
-  isPending: boolean;
-}) => {
-  return (
-    <div className="min-h-full grid grid-cols-4 gap-3 place-items-center">
-      {isPending ? (
-        <p className="w-full text-xl text-center">Updating the list</p>
-      ) : (
-        products.map((product, index) => <Product key={index} name={product} />)
-      )}
-    </div>
-  );
-};
-
-const products = getProducts();
-
-const filteringProduct = (filterConstraint: string) => {
-  return filterConstraint
-    ? products.filter((product) => product.includes(filterConstraint))
-    : products;
-};
+const accordions = [
+  {
+    id: uuid4(),
+    title: "What is HTML5",
+    isActive: false,
+    content: (
+      <p className="text-orange-600">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eum quis
+        invidunt id facilisis cillum, excepteur quis wisi eiusmod velit id te
+        qui possim ipsum. Consequat eleifend euismod duo. Ipsum ex quis sanctus
+        feugiat option doming luptatum nostrud voluptua tempor. Eiusmod delenit
+        est. Esse vulputate option.
+      </p>
+    ),
+  },
+  {
+    id: uuid4(),
+    title: "What's CSS",
+    isActive: false,
+    content: (
+      <p className="text-slate-400">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eum quis
+        invidunt id facilisis cillum, excepteur quis wisi eiusmod velit id te
+        qui possim ipsum. Consequat eleifend euismod duo. Ipsum ex quis sanctus
+        feugiat option doming luptatum nostrud voluptua tempor. Eiusmod delenit
+        est. Esse vulputate option.
+      </p>
+    ),
+  },
+  {
+    id: uuid4(),
+    title: "What's Javascript",
+    isActive: false,
+    content: (
+      <p className="text-blue-600">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eum quis
+        invidunt id facilisis cillum, excepteur quis wisi eiusmod velit id te
+        qui possim ipsum. Consequat eleifend euismod duo. Ipsum ex quis sanctus
+        feugiat option doming luptatum nostrud voluptua tempor. Eiusmod delenit
+        est. Esse vulputate option.
+      </p>
+    ),
+  },
+  {
+    id: uuid4(),
+    title: "What's React",
+    isActive: false,
+    content: (
+      <p className="text-green-400">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eum quis
+        invidunt id facilisis cillum, excepteur quis wisi eiusmod velit id te
+        qui possim ipsum. Consequat eleifend euismod duo. Ipsum ex quis sanctus
+        feugiat option doming luptatum nostrud voluptua tempor. Eiusmod delenit
+        est. Esse vulputate option.
+      </p>
+    ),
+  },
+  {
+    id: uuid4(),
+    title: "What's Angular",
+    isActive: false,
+    content: (
+      <p className="text-white">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eum quis
+        invidunt id facilisis cillum, excepteur quis wisi eiusmod velit id te
+        qui possim ipsum. Consequat eleifend euismod duo. Ipsum ex quis sanctus
+        feugiat option doming luptatum nostrud voluptua tempor. Eiusmod delenit
+        est. Esse vulputate option.
+      </p>
+    ),
+  },
+];
 
 function App() {
-  const [isPending, startTransition] = useTransition();
-
-  const [filterConstraint, setFilterConstraint] = useState<string>("");
-
-  const filteredProducts = useMemo(
-    () => filteringProduct(filterConstraint),
-    [filterConstraint]
-  );
-
-  const handleFiltering = (e: ChangeEvent<HTMLInputElement>) => {
-    startTransition(() => {
-      setFilterConstraint(e.target.value);
-    });
-  };
-
   return (
-    <div className="flex flex-col items-center justify-between gap-2">
-      <div>
-        <label className="block text-xl" htmlFor="search">
-          Search
-        </label>
-        <input
-          id="search"
-          className="w-[450px] h-[35px] border border-stone-200 outline-none p-3 rounded-full mb-1"
-          type="search"
-          value={filterConstraint}
-          onChange={handleFiltering}
-        />
-      </div>
-      <ProductList isPending={isPending} products={filteredProducts} />
+    <div className="flex justify-center items-center">
+      <Accordions accordions={accordions} />
     </div>
   );
 }
