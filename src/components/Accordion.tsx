7bf0fa22a10e8c959df838e46f03912273839c88
Djioleu Florian <nsiegningflorian@gmail.com>
2024-02-02 11:48:52 +0100

    Add the Accordion Prop type

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,3 +1,13 @@
+import { ReactNode } from "react";
+
+export interface AccordionProps {
+  id: string;
+  title: string;
+  isActive: boolean;
+  content: ReactNode;
+  handleClick: (id: string) => void;
+}
+
 const Accordions = () => {
   return <div>Accordion</div>;
 };
