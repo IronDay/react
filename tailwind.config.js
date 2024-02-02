11d59dd841aafa55a6303b4947fe4ab94ec69939
Djioleu Florian <nsiegningflorian@gmail.com>
2024-02-02 11:59:59 +0100

    end of the accordion implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   fontFamily: {
-    sans: ['Inter', 'sans-serif'],
-    serif: ['Roboto', 'serif'],
+    sans: "Inter, sans-serif",
+    serif: "Roboto, serif",
+    inter: "Inter, sans-serif",
+    robotoSlab: "Roboto Slab, sans-serif",
+    roboto: "Roboto, sans-serif",
   },
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
