a5fafc8bac2af9857f6ae53221ec92bfa341b374
Djioleu Florian <nsiegningflorian@gmail.com>
2024-02-02 11:50:13 +0100

    Implement the single Accordion component

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
 export interface AccordionProps {
   id: string;
@@ -7,6 +8,24 @@ export interface AccordionProps {
   content: ReactNode;
   handleClick: (id: string) => void;
 }
+
+export const Accordion = ({
+  title,
+  content,
+  isActive,
+  id,
+  handleClick,
+}: AccordionProps) => {
+  return (
+    <div className="w-full" onClick={() => handleClick(id)}>
+      <div className="min-h-[20px] bg-purple-950 p-3 text-white rounded flex items-center justify-between">
+        <h2 className="font-[inter]">{title}</h2>
+        {isActive ? <IoIosArrowUp /> : <IoIosArrowDown />}
+      </div>
+      {isActive && <div className="p-3 font-[roboto]">{content}</div>}
+    </div>
+  );
+};
 
 const Accordions = () => {
   return <div>Accordion</div>;
