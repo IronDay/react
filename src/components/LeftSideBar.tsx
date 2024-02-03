0a0fa709086dbd7a3a8ec47631489c4bfac26301
Djioleu Florian <nsiegningflorian@gmail.com>
2024-02-03 18:17:57 +0100

    Implement the Group component

--- src/components/LeftSideBar.tsx
+++ src/components/LeftSideBar.tsx
@@ -4,3 +4,12 @@ interface GroupProps {
   title: string;
   children: ReactNode;
 }
+
+const Group = ({ title, children }: GroupProps) => {
+  return (
+    <div>
+      <h3>{title}</h3>
+      {children}
+    </div>
+  );
+};
