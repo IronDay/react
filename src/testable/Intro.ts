9106c02fcf47199ab0b4627004b6b6ac661a2069
Djioleu Florian <nsiegningflorian@gmail.com>
2024-02-07 20:43:19 +0100

    Add the test for the second branch of the code,then correct the issue

--- src/testable/Intro.ts
+++ src/testable/Intro.ts
@@ -1,5 +1,5 @@
 export function max(lhs: number, rhs: number) {
   if (lhs > rhs) return lhs;
-  else if (lhs < rhs) return lhs;
+  else if (lhs < rhs) return rhs;
   else return lhs;
 }
--- tests/Intro.test.ts
+++ tests/Intro.test.ts
@@ -5,4 +5,8 @@ describe("Test of the intro function", () => {
   it("should return the first when it's the max", () => {
     expect(max(4, 2)).toBe(4);
   });
+
+  it("should return the second value if it's the max", () => {
+    expect(max(2, 4)).toBe(4);
+  });
 });
