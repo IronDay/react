04e92cb8ebe21bd55cb5933f3d82a74733ba7e1f
Djioleu Florian <nsiegningflorian@gmail.com>
2024-02-07 20:48:53 +0100

    Refactoring: Use the ternary operator to improve the code and reach 100% of test coverage

--- src/testable/Intro.ts
+++ src/testable/Intro.ts
@@ -1,5 +1,4 @@
 export function max(lhs: number, rhs: number) {
-  if (lhs > rhs) return lhs;
-  else if (lhs < rhs) return rhs;
-  else return lhs;
+  if (lhs === rhs) return lhs;
+  return lhs > rhs ? lhs : rhs;
 }
