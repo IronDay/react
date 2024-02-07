4c6d2ffe34cd78a09d0fe9495861b3d38c7db52e
Djioleu Florian <nsiegningflorian@gmail.com>
2024-02-07 20:46:12 +0100

    Add the test for the case of same number comparison

--- tests/Intro.test.ts
+++ tests/Intro.test.ts
@@ -9,4 +9,8 @@ describe("Test of the intro function", () => {
   it("should return the second value if it's the max", () => {
     expect(max(2, 4)).toBe(4);
   });
+
+  it("should return the first value as the max if the two values are equal", () => {
+    expect(max(2, 2)).toBe(2);
+  });
 });
