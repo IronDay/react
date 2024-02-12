6f798f816aaadc14a832fec71156f0934a852828
Djioleu Florian <nsiegningflorian@gmail.com>
2024-02-12 19:45:00 +0100

    Implement a simple test of the useReducer Hooks

--- src/components/Reducer.tsx
+++ src/components/Reducer.tsx
@@ -1,3 +1,5 @@
+import { useReducer } from "react";
+
 interface State {
   count: number;
 }
@@ -12,3 +14,28 @@ const reducer = (state: State, action: Action) => {
       return { ...state, count: state.count - 1 };
   }
 };
+
+const Reducer = () => {
+  const [{ count }, dispatch] = useReducer(reducer, { count: 0 });
+  return (
+    <div className=" min-h-[100vh] flex flex-col gap-3 justify-center items-center">
+      <p>{count}</p>
+      <div className="flex gap-3">
+        <button
+          onClick={() => dispatch("Increment")}
+          className="bg-purple-400 p-3 rounded"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => dispatch("Decrement")}
+          className="bg-stone-400 p-3 rounded"
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Reducer;
