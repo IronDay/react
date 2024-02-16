4f614df8f3dd30a648e7a7f1896b39a3ab6715c7
Djioleu Florian <nsiegningflorian@gmail.com>
2024-02-16 23:09:10 +0100

    Combine the zustand store with a reducer to update the state

--- src/components/Zustands.tsx
+++ src/components/Zustands.tsx
@@ -1,22 +1,23 @@
-import { CounterStore } from "../stores/CounterStore.ts";
+import { useCounterStore } from "../stores/CounterStore.ts";
 
 const Zustands = () => {
-  const count = CounterStore((state) => state.count);
-  const increment = CounterStore((state) => state.increment);
-  const decrement = CounterStore((state) => state.decrement);
+  const count = useCounterStore((state) => state.count);
+  const dispatch = useCounterStore((state) => state.dispatch);
+  /*const increment = CounterStore((state) => state.increment);
+      const decrement = CounterStore((state) => state.decrement);*/
 
   return (
     <div className=" min-h-[100vh] flex flex-col gap-3 justify-center items-center">
       <p className="font-[roboto] font-bold">{count}</p>
       <div className="flex gap-3">
         <button
-          onClick={increment}
+          onClick={() => dispatch({ type: "increment", step: 4 })}
           className="border border-blue-400 rounded p-3 capitalize font-[roboto]"
         >
           increment
         </button>
         <button
-          onClick={decrement}
+          onClick={() => dispatch({ type: "decrement", step: 2 })}
           className="border border-stone-400 p-3 rounded capitalize font-[roboto]"
         >
           decrement
--- src/stores/CounterStore.ts
+++ src/stores/CounterStore.ts
@@ -1,15 +1,39 @@
-interface State {
-  count: number;
-}
-interface Actions {
-  increment: () => void;
-  decrement: () => void;
-}
+import { create } from "zustand";
 
-export const CounterStore = create<State & Actions>()((set) => {
+type State = {
+  count: number;
+};
+
+type Action = {
+  type: "increment" | "decrement";
+  step: number;
+};
+
+type Actions = {
+  /* increment: (action: Action) => void;
+  decrement: (action: Action) => void;*/
+  dispatch: (action: Action) => void;
+};
+const countReducers = (state: State, action: Action) => {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + action.step };
+    case "decrement":
+      return { count: state.count - action.step };
+    default:
+      return state;
+  }
+};
+
+/*export const CounterStore = create<State & Actions>()((set) => {
   return {
     count: 0,
     increment: () => set((state) => ({ count: state.count + 1 })),
     decrement: () => set((state) => ({ count: state.count - 1 })),
   };
-});
+});*/
+
+export const useCounterStore = create<State & Actions>((set) => ({
+  count: 0,
+  dispatch: (action: Action) => set((state) => countReducers(state, action)),
+}));
